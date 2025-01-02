888668835da7eaf215c990898f7e48c6948c72e7
psychAura <kausara931@gmail.com>
2025-01-02 06:37:40 +0000

    Refactor home route to paginate posts and update deprecated count method

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,21 +3,55 @@ const router = express.Router();
 const Post = require('../models/post');
 
 
-//Routes
+//Routes GET HOME
 
 //GET - HOME ROUTE
 router.get('', async(req, res) =>{
+    try {
     const locals = {
         title: 'nodejs blog',
         description: 'simple boggdhjasdfvgygd'
     }
-    try{
-        const data = await Post.find();
-        res.render('index', {locals, data});
-    } catch(error){
-        console.error(error)
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+    // Count is deprecated - please use countDocuments
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+        currentRoute: '/'
+    });
+
+    } catch (error) {
+    console.log(error);
     }
-});
+
+    });
+
+// router.get('', async(req, res) =>{
+//     const locals = {
+//         title: 'nodejs blog',
+//         description: 'simple boggdhjasdfvgygd'
+//     }
+//     try{
+//         const data = await Post.find();
+//         res.render('index', {locals, data});
+//     } catch(error){
+//         console.error(error)
+//     }
+// });
 
 
 router.get('/about', (req, res) =>{
